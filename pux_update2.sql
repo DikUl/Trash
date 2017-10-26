11dce62ea0b702487fdcb259e7fde686de5df8fc
DikUln <dik.ulanovka@gmail.com>
2017-10-26 18:24:36 +0300

    Update pux_update2.sql

--- pux_update2.sql
+++ pux_update2.sql
@@ -1,8 +1,11 @@
-select
-        q.attr1, q.attr2, q.adddate, percentile_disc(0.5) within group(order by qj.cost)
+select v.* from
+(
+   select
+        q.attr1, q.attr2, q.adddate, percentile_disc(0.5) within group(order by qj.cost) over (partition by q.attr1, q.attr2, q.adddate) as perc
    from plv_test_table q
    inner join plv_test_table qj
            on q.attr1 = qj.attr1
           and q.attr2 = qj.attr2
           and qj.adddate between q.adddate - 30 and q.adddate
-   group by q.attr1, q.attr2, q.adddate
+) v
+group by v.attr1, v.attr2, v.adddate, v.perc
